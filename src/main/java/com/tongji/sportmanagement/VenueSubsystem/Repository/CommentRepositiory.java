46b2386b4c8461726d67cf18a636702522d20721
王捷 <14271162+wang-jie2310@user.noreply.gitee.com>
2024-12-31 22:42:41 +0800

    场地评价v3 - 未连接user

--- src/main/java/com/tongji/sportmanagement/VenueSubsystem/Repository/CommentRepositiory.java
+++ src/main/java/com/tongji/sportmanagement/VenueSubsystem/Repository/CommentRepositiory.java
@@ -2,11 +2,15 @@ package com.tongji.sportmanagement.VenueSubsystem.Repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.tongji.sportmanagement.VenueSubsystem.Entity.VenueComment;
 
 public interface CommentRepositiory extends CrudRepository<VenueComment, Integer>
 {
-  @Query
-  Iterable<VenueComment> findAllByVenueId(Integer venueId);
+  @Query(value = "SELECT * FROM venue_comment WHERE venue_id = :venueId LIMIT :count OFFSET :offset", nativeQuery = true)
+  Iterable<VenueComment> findCommentByVenueId(@Param("venueId") Integer venueId, @Param("offset") long offset, @Param("count") int count);
+
+  @Query(value = "SELECT COUNT(*) FROM venue_comment WHERE venue_id = :venueId", nativeQuery = true)
+  long getCommentCount(@Param("venueId") Integer venueId);
 }
