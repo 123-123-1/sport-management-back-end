8b990c2998c86becdecd85c71d21991644a520a4
王捷 <14271162+wang-jie2310@user.noreply.gitee.com>
2025-01-01 15:32:27 +0800

    场馆子系统重构：Service拆分与controller逻辑

--- src/main/java/com/tongji/sportmanagement/VenueSubsystem/Controller/VenueController.java
+++ src/main/java/com/tongji/sportmanagement/VenueSubsystem/Controller/VenueController.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tongji.sportmanagement.Common.ServiceException;
+import com.tongji.sportmanagement.Common.DTO.ErrorMsg;
 import com.tongji.sportmanagement.VenueSubsystem.DTO.PostCommentDTO;
+import com.tongji.sportmanagement.VenueSubsystem.Service.CommentService;
+import com.tongji.sportmanagement.VenueSubsystem.Service.CourtService;
+import com.tongji.sportmanagement.VenueSubsystem.Service.TimeslotService;
 import com.tongji.sportmanagement.VenueSubsystem.Service.VenueService;
 
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,46 +24,91 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class VenueController 
 {
   @Autowired
-  private VenueService service;
+  private VenueService venueService;
+  @Autowired
+  private CourtService courtService;
+  @Autowired
+  private TimeslotService timeslotService;
+  @Autowired
+  private CommentService commentService;
 
   @GetMapping("/list")
   public ResponseEntity<Object> getVenueList(@RequestParam int page, @RequestParam String name)
   {
-    return service.getAllVenues(page, name);
+    try{
+      return ResponseEntity.ok().body(venueService.getAllVenues(page, name));
+    }
+    catch(Exception e){
+      return ResponseEntity.internalServerError().body(new ErrorMsg(e.getMessage()));
+    }
   }
 
   @GetMapping("/detail")
   public ResponseEntity<Object> getVenueDetail(@RequestParam int venueId)
   {
-    return service.getVenueDetail(venueId);
+    try{
+      return ResponseEntity.ok().body(venueService.getVenueDetail(venueId));
+    }
+    catch(ServiceException e){
+      return ResponseEntity.status(e.getCode()).body(new ErrorMsg(e.getMessage()));
+    }
+    catch(Exception e){
+      return ResponseEntity.internalServerError().body(new ErrorMsg(e.getMessage()));
+    }
   }
 
   @GetMapping("/courts")
   public ResponseEntity<Object> getVenueCourts(@RequestParam int venueId)
   {
-    return service.getVenueCourts(venueId);
+    try{
+      return ResponseEntity.ok().body(courtService.getVenueCourts(venueId));
+    }
+    catch(Exception e){
+      return ResponseEntity.internalServerError().body(new ErrorMsg(e.getMessage()));
+    }
   }
 
   @GetMapping("/timeslots")
   public ResponseEntity<Object> getVenueTimeslots(@RequestParam int venueId, @RequestParam String date)
   {
-    return service.getVenueTimeslots(venueId, date);
+    try{
+      return ResponseEntity.ok().body(timeslotService.getVenueTimeslots(venueId, date));
+    }
+    catch(Exception e){
+      return ResponseEntity.internalServerError().body(new ErrorMsg(e.getMessage()));
+    }
   }
 
   @GetMapping("/comments")
   public ResponseEntity<Object> getVenueComments(@RequestParam int venueId, @RequestParam long page)
   {
-    return service.getVenueComments(venueId, page);
+    try{
+      return ResponseEntity.ok().body(commentService.getVenueComments(venueId, page));
+    }
+    catch(Exception e){
+      return ResponseEntity.internalServerError().body(new ErrorMsg(e.getMessage()));
+    }
   }
 
   @PostMapping("/comments")
   // 等待JWT相关功能
   // public ResponseEntity<Object> postVenueComment(@RequestBody PostCommentDTO comment, @RequestAttribute int userId)
   // {
-  //   return service.postVenueComment(comment, userId);
+  //   try{
+  //     return ResponseEntity.ok().body(commentService.postVenueComment(comment, userId));
+  //   }
+  //   catch(Exception e){
+  //     return ResponseEntity.internalServerError().body(new ErrorMsg(e.getMessage()));
+  //   }
   // }
+
   public ResponseEntity<Object> postVenueComment(@RequestBody PostCommentDTO comment)
   {
-    return service.postVenueComment(comment, 1);
+    try{
+      return ResponseEntity.ok().body(commentService.postVenueComment(comment, 1));
+    }
+    catch(Exception e){
+      return ResponseEntity.internalServerError().body(new ErrorMsg(e.getMessage()));
+    }
   }
 }
