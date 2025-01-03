7ed5c15acc31349af6232c3ccc32bf2b5f2f69ba
wfy3364 <2489589935@qq.com>
2025-01-03 19:51:45 +0800

    修改通知排序

--- src/main/java/com/tongji/sportmanagement/AccountSubsystem/Repository/NotificationRepository.java
+++ src/main/java/com/tongji/sportmanagement/AccountSubsystem/Repository/NotificationRepository.java
@@ -1,12 +1,10 @@
 package com.tongji.sportmanagement.AccountSubsystem.Repository;
 
 import com.tongji.sportmanagement.AccountSubsystem.Entity.Notification;
-import com.tongji.sportmanagement.AccountSubsystem.Entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface NotificationRepository extends CrudRepository<Notification, Integer> {
+    @Query(value = "select * from notification where user_id=:userId order by timestamp desc", nativeQuery = true)
     Iterable<Notification> findAllByUserId(int userId);
 }
